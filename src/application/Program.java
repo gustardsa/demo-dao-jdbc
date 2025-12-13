b895211436ded394820f4917daf9b847852257a2
gustardsa <gustardsa@gmail.com>
2025-12-13 10:45:05 -0300

    Implementado a funcao insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,5 +30,10 @@ public class Program {
             System.out.println(obj);
         }
 
+        System.out.println("\n=== Teste 4: Seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
+
     }
 }
